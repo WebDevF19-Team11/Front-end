2afcef9231e0917666cdb7e6e70eca610f2fa304
Ali <mobdea@gmail.com>
2019-11-29 15:50:46 -0500

    fix: register page lables

--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -8,31 +8,49 @@ export default class RegisterPage extends Component {
     render() {
         return (
             <div class="container-fluid  t11-container t11-Page">
-                <h1 className="t11-title">Sign Up</h1>
+                <h1 className="t11-title">Register</h1>
                 <br />
                 <div class="container">
                     <form className="">
                         <div className="row form-group">
-                            <div className="col">
-                                <input type="text" className="form-control" placeholder="First name" />
+                            <div className="col t11-register-col">
+                                <label for="first-name">First name</label>
+                                <input id="first-name"
+                                        type="text" 
+                                        className="form-control" 
+                                        placeholder="First name" />
                             </div>
-                            <div className="col">
-                                <input type="text" className="form-control" placeholder="Last name" />
+                            <div className="col t11-register-col">
+                                <label for="Last-name">Last name</label>
+                                <input id="Last-name" 
+                                        type="text" 
+                                        className="form-control" 
+                                        placeholder="Last name"/>
                             </div>
                         </div>
-                        <div className="form-group">
+                        <div className="form-group t11-register-col">
+                        <label for="username">Username</label>
+                            <input type="text"
+                                className="form-control"
+                                id="username"
+                                name="usename"
+                                placeholder="username" />
+                        </div>
+                        <div className="form-group t11-register-col">
+                        <label for="signup-emailId">Email</label>
                             <input type="text"
                                 className="form-control"
                                 id="signup-emailId"
                                 name="email"
-                                placeholder="Your email.." />
+                                placeholder="Email" />
                         </div>
-                        <div className="form-group">
+                        <div className="form-group t11-register-col">
+                        <label for="signup-passwordId">Password</label>
                             <input type="text"
                                 className="form-control"
                                 id="signup-passwordId"
                                 name="password"
-                                placeholder="Your password.." />
+                                placeholder="Password" />
                         </div>
                         <button type="button" class="btn btn-light">Submit</button>
                     </form>
---
+++ src/components/profilePage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default class ProfilePage extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return ( 
+       <div>
+
+       </div>
+        );
+    }
+
+}
